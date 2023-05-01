3374c4aae189ac72ec981cbef803b5c61d195199
luisferreira22 <luisferreira22@hotmail.com>
2023-05-01 16:51:31 -0300

    Test Conecction with GitHub

---
+++ BlazorCRUD.Model/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Model
+{
+   public  class Client
+    {
+        public int ClientId { get; set; }
+     
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime BornDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Picture { get; set; }
+    }
+}
